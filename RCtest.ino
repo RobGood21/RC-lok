af907f1475a013b82fb71c25097c743f920afa39
Rob Antonisse <robantonisse@gmail.com>
2021-03-22 09:33:36 +0100

    RC test

--- RCtest.ino
+++ RCtest.ino
@@ -3,12 +3,19 @@
     Created:	22-3-2021 9:22:44
     Author:     Rob Antonisse
 
-	Testen met rc gebruk makend van een NRF24L01 mini smd
+	Testen met rc gebruik makend van een NRF24L01 mini smd
 
 
 
 
 */
+
+
+#include <SPI.h>
+#include <RF24_config.h>
+#include <RF24.h>
+#include <printf.h>
+#include <nRF24L01.h>
 
 
 void setup()
